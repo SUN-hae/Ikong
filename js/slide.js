eea289a84a70ac5866147862a704c36c2029cb3c
SUN-hae <sunhae4486@gmail.com>
2025-06-10 18:04:30 +0900

    2025.06.10

--- js/slide.js
+++ js/slide.js
@@ -2,7 +2,8 @@ let swiper = new Swiper(".swiper", {
   cssMode: true, //옵션
   slidesPerView: 3, // 한 슬라이드에 보여줄 갯수
   slidesPerGroup: 3, //한번에 슬라이딩될 개수
-  spaceBetween: 20, //아이템 사이 간격
+  spaceBetween: 40, //아이템 사이 간격
+  loop: true,
   navigation: {
     //양쪽 화살표
     nextEl: ".swiper-button-next",

039241b379f3901d7dbfd98a435c761eea7a60a3
SUN-hae <sunhae4486@gmail.com>
2025-06-12 17:27:41 +0900

    [docs style] sec4까지 완성

--- js/slide.js
+++ js/slide.js
@@ -16,3 +16,30 @@ let swiper = new Swiper(".swiper", {
   mousewheel: true,
   keyboard: true,
 });
+
+function updateTimer() {
+  const future = Date.parse("2025/07/25 18:20:00");
+  const now = new Date();
+  const diff = future - now;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor(diff / (1000 * 60 * 60));
+  const mins = Math.floor(diff / (1000 * 60));
+  const secs = Math.floor(diff / 1000);
+
+  const d = days;
+  const h = hours - days * 24;
+  const m = mins - hours * 60;
+  const s = secs - mins * 60;
+  document.getElementById("timer").innerHTML =
+    "<h5>남은 시간</h5>" +
+    d +
+    "<span>일 : </span>" +
+    h +
+    "<span> : </span>" +
+    m +
+    "<span> : </span>" +
+    s;
+}
+
+setInterval(updateTimer, 1000);
